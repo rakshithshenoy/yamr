d063c7e97413c0925fc2e438f66d0d42b8ed6116
Rakshith Shenoy <rakshithshenoy@outlook.com>
2022-11-29 22:31:00 +0530

    update worker node Co-authored-by: Divya-Niranjan <Divya-Niranjan@users.noreply.github.com> Co-authored-by: Noel Saju <Noel-Saju@users.noreply.github.com>

--- readme.md
+++ readme.md
@@ -1,3 +1,38 @@
 - To view the contents of the folder within the docker container, run the following command
 `sudo docker cp yet_another_map_reduce-worker_node-3-1:/app/data3 ~/projects/yet_another_map_reduce/worker_node`
 
+---
+# Routes
+### Write Operation
+
+On master node - 
+  `POST /splitnormal` - Receives req.files.masterfile
+On worker node - 
+  `POST /data` - Receives req.body.arr req.body.num req.body.name
+
+
+### Read Operation
+
+On master node - 
+  `GET /read/:name` - :name should be a file of the format hello.txt
+
+On worker node - 
+  `POST /read` - Receives req.body.num req.body.name
+
+
+### Mapper
+
+On master node -
+`POST /mapper/:name` - :name should be a file of the format hello.txt
+
+On worker node- 
+`POST /mapper/` - req.body.name should be a file of the format hello
+
+### Shuffling function(partition)
+
+On master node -
+`GET /shuffle/:name` - :name should be a file of the format hello.txt
+
+On worker node -
+`POST /shuffledata` - Receives req.body.arr req.body.num req.body.name
+  
--- worker_node/index.js
+++ worker_node/index.js
@@ -7,17 +7,30 @@ app.use(express.urlencoded());
 
 app.post("/data", (req, res) => {
   try {
-    console.log(req.body);
     const array = req.body.arr;
     const num = req.body.num;
     const name = req.body.name;
     const data = JSON.stringify(array);
-    console.log("DATA", data, num, name);
     const f = fs.writeFileSync(
       `./data${num}/${name.slice(0, -4)}-${num}.txt`,
       data
     );
-    console.log("WRITTEN", f);
+    return res.status(200).json({ message: "Written successfully" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post("/shuffledata", (req, res) => {
+  try {
+    const array = req.body.arr;
+    const num = req.body.num;
+    const name = req.body.name;
+    const data = JSON.stringify(array);
+    const f = fs.writeFileSync(
+      `./data${num}/${name.slice(0, -4)}-shuffled-${num}.txt`,
+      data
+    );
     return res.status(200).json({ message: "Written successfully" });
   } catch (err) {
     res.status(500).send(err);
@@ -30,9 +43,78 @@ app.post("/read", (req, res) => {
 
     const data = fs.readFileSync(`/app/data${num}/${req.body.name}-${num}.txt`);
 
-    console.log(data);
     const arr = JSON.parse(data);
     res.status(200).json({ arr });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post("/mapper", (req, res) => {
+  try {
+    const num = req.body.num;
+    const name = req.body.name;
+    const data = fs.readFileSync(`/app/data${num}/${name}-${num}.txt`);
+    const arr = JSON.parse(data);
+    let mappedArr = arr.map((ele) => {
+      let obj = {
+        key: `${ele}`,
+        value: 1,
+      };
+      return obj;
+    });
+    const mappedData = JSON.stringify(mappedArr);
+    const f = fs.writeFileSync(
+      `./data${num}/${name}-mapper-${num}.txt`,
+      mappedData
+    );
+
+    res.status(200).json({ mappedArr });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post("/reducer", (req, res) => {
+  try {
+    const num = req.body.num;
+    const name = req.body.name;
+    const data = fs.readFileSync(`/app/data${num}/${name}-shuffled-${num}.txt`);
+    const arr = JSON.parse(data);
+
+    const reducedArr = [];
+    let count = 1;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i].key != arr[i + 1].key) {
+        const obj = {
+          key: arr[i].key,
+          count,
+        };
+        reducedArr.push(obj);
+        count = 1;
+      } else {
+        count++;
+      }
+    }
+
+    if (count > 1) {
+      const obj = {
+        key: arr[arr.length - 1].key,
+        count,
+      };
+      reducedArr.push(obj);
+    } else {
+      const obj = {
+        key: arr[arr.length - 1].key,
+        count: 1,
+      };
+      reducedArr.push(obj);
+    }
+
+    const reducedData = JSON.stringify(reducedArr);
+    fs.writeFileSync(`./data${num}/${name}-reduced-${num}.txt`, reducedData);
+
+    res.status(200).json({ reducedArr });
   } catch (err) {
     res.status(500).send(err);
   }
